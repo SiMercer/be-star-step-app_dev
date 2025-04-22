84cf60dd3db3d881f37523303a63c1739798b78d
SiMercer <na>
2025-04-22 10:45:12 +0100

    getRewardsByParentID added

--- models/rewards.models.js
+++ models/rewards.models.js
@@ -1,77 +1,61 @@
-const { Rewards } = require("../db/test_data/test.schema");
+const { Reward } = require("../db/test_data/test.schema");
 
-exports.selectRewardById = (id) => {
-  return Rewards.findById(id).then(
-    ({ _id, title, cost, redeemedBy, isRedeemed, createdBy }) => {
-      return {
-        reward_id: _id,
-        title,
-        cost,
-        redeemedBy,
-        isRedeemed,
-        createdBy,
-      };
-    }
-  );
-};
-exports.selectRewards = (queries) => {
-  if (queries.createdBy) {
-    return Rewards.find({ createdBy: `${queries.createdBy}` }).then(
-      (rewards) => {
-        if (rewards.length > 0) {
-          return rewards.map(
-            ({ _id, title, cost, redeemedBy, isRedeemed, createdBy }) => {
-              return {
-                reward_id: _id,
-                title,
-                cost,
-                redeemedBy,
-                isRedeemed,
-                createdBy,
-              };
-            }
-          );
-        }
-      }
-    );
-  } else {
-    return Promise.reject({
-      status: 400,
-      msg: "Bad Request - use createdBy query",
-    });
+exports.selectRewardById = async (id) => {
+  const doc = await Reward.findById(id);
+  if (!doc) {
+    const err = new Error("Reward not found");
+    err.status = 404;
+    throw err;
   }
+  const { _id, title, cost, redeemedBy, isRedeemed, createdBy } = doc;
+  return { reward_id: _id, title, cost, redeemedBy, isRedeemed, createdBy };
 };
-exports.createRewards = (body) => {
-  body.isRedeemed = false;
 
-  return Rewards.create(body).then(
-    ({ _id, title, cost, isRedeemed, createdBy }) => {
-      return {
-        reward_id: _id,
-        title,
-        cost,
-        isRedeemed,
-        createdBy,
-      };
-    }
+exports.selectRewards = async (queries) => {
+  if (!queries.createdBy) {
+    const err = new Error("Bad Request – use createdBy query");
+    err.status = 400;
+    throw err;
+  }
+  const docs = await Reward.find({ createdBy: queries.createdBy });
+  return docs.map(
+    ({ _id, title, cost, redeemedBy, isRedeemed, createdBy }) => ({
+      reward_id: _id,
+      title,
+      cost,
+      redeemedBy,
+      isRedeemed,
+      createdBy,
+    })
   );
 };
-exports.updateRewardsById = (reward_id, body) => {
-  return Rewards.findByIdAndUpdate(reward_id, body, { new: true }).then(
-    ({ _id, title, cost, redeemedBy, isRedeemed, createdBy }) => {
-      return {
-        reward_id: _id,
-        title,
-        cost,
-        redeemedBy,
-        isRedeemed,
-        createdBy,
-      };
-    }
-  );
+
+exports.createRewards = async (body) => {
+  body.isRedeemed = false;
+  const doc = await Reward.create(body);
+  const { _id, title, cost, isRedeemed, createdBy } = doc;
+  return { reward_id: _id, title, cost, isRedeemed, createdBy };
 };
-exports.removeRewardsById = (reward_id) => {
-  return Rewards.findByIdAndDelete(reward_id);
+
+exports.updateRewardsById = async (reward_id, body) => {
+  const doc = await Reward.findByIdAndUpdate(reward_id, body, { new: true });
+  if (!doc) {
+    const err = new Error("Reward not found");
+    err.status = 404;
+    throw err;
+  }
+  const { _id, title, cost, redeemedBy, isRedeemed, createdBy } = doc;
+  return { reward_id: _id, title, cost, redeemedBy, isRedeemed, createdBy };
+};
+
+exports.removeRewardsById = async (reward_id) => {
+  const doc = await Reward.findByIdAndDelete(reward_id);
+  if (!doc) {
+    const err = new Error("Reward not found");
+    err.status = 404;
+    throw err;
+  }
+  return { reward_id: doc._id };
 };
 
 exports.getRewardsByParentId = async (parentID) => {
@@ -80,5 +64,15 @@ exports.getRewardsByParentId = async (parentID) => {
     err.status = 400;
     throw err;
   }
-  return Reward.find({ createdBy: parentID });
+  const docs = await Reward.find({ createdBy: parentID });
+  return docs.map(
+    ({ _id, title, cost, redeemedBy, isRedeemed, createdBy }) => ({
+      reward_id: _id,
+      title,
+      cost,
+      redeemedBy,
+      isRedeemed,
+      createdBy,
+    })
+  );
 };
